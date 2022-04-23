b9fa9e843196a74d662ddba450460aa907909fe0
Talerchik Alexey <talerchik@protonmail.com>
2022-04-23 09:41:40 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,10 +14,26 @@ window.onload = function () {
   var a = document.querySelector("#header-button-services");
   //вешаем на него события
   a.onmouseout = function (e) {
-    document.getElementById("arrow").setAttribute('fill', '#FFFFFF');
+    document.getElementById("arrow").setAttribute("fill", "#FFFFFF");
   };
 
   a.onmouseover = function (e) {
-    document.getElementById("arrow").setAttribute('fill', '#19191B');
+    document.getElementById("arrow").setAttribute("fill", "#19191B");
+  };
+
+  document.querySelector(".contact").onclick = function () {
+    document.querySelector(".contact-form").classList.remove("visually-hidden");
+  };
+
+  document.querySelector(".close-button").onclick = function () {
+    document.querySelector(".contact-form").classList.add("visually-hidden");
   };
 };
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
